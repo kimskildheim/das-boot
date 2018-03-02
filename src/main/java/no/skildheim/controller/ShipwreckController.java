b06629c4f48beb7902cf82d7f16475d862ed862f
Kim Skildheim <kimskildheim@gmail.com>
2018-03-02 12:39:29 +0100

    First commit

---
+++ src/main/java/no/skildheim/controller/ShipwreckController.java
@@ -0,0 +1,36 @@
+package no.skildheim.controller;
+
+import no.skildheim.model.Shipwreck;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+public class ShipwreckController {
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
+    public List<Shipwreck> list(){
+        return ShipwreckStub.list();
+    }
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.POST)
+    public Shipwreck create(@RequestBody Shipwreck shipwreck){
+        return ShipwreckStub.create(shipwreck);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+    public Shipwreck get(@PathVariable Long id){
+        return ShipwreckStub.get(id);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
+    public Shipwreck put(@PathVariable Long id, @RequestBody Shipwreck shipwreck){
+        return ShipwreckStub.update(id, shipwreck);
+    }
+
+    @RequestMapping(value = "sjipwrecks/{id}", method = RequestMethod.DELETE)
+    public Shipwreck delete(@PathVariable Long id){
+        return ShipwreckStub.delete(id);
+    }
+}
